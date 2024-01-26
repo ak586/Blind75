cf960da95a17049529ea562e1f1ee52087a9cf1a
Ajit Kumar <78835344+ak586@users.noreply.github.com>
2024-01-26 23:18:56 +0530

    Time: 42 ms (14.54%) | Memory: 42.7 MB (45.72%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,5 +1,7 @@
 class Solution {
-    public int characterReplacement(String s, int k) {
+
+    // brute force
+    public int characterReplacement2(String s, int k) {
         int n=s.length();
         int ans=0;
         for(int i=0; i<n; i++){
@@ -15,6 +17,31 @@ class Solution {
                 } 
                 ans=Math.max(currAns, ans);
             }
+        }
+        return ans;
+    }
+
+    public int maxF(int freqMap[]){
+        int max=0;
+        for(int a: freqMap) max=Math.max(max, a);
+        return max;
+    }
+    public int characterReplacement(String s, int k) {
+        int n=s.length();
+        int i=0;
+        int j=0;
+        int freqMap[]=new int[256];
+        int ans=0;
+        while(j<n){
+            freqMap[s.charAt(j)]++;
+            int currAns=j-i+1;
+            while(currAns-maxF(freqMap)>k && i<=j){
+                freqMap[s.charAt(i)]--;
+                i++;
+                currAns=j-i+1;
+            }
+            ans=Math.max(currAns, ans);
+            j++;
         }
         return ans;
     }
