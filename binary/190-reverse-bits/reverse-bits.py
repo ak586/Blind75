d8789340e21d7ba659bba35cae481f0b6b47319c
Ajit Kumar <78835344+ak586@users.noreply.github.com>
2024-01-28 12:23:25 +0530

    Time: 19 ms (100.00%) | Memory: 16.5 MB (66.44%) - LeetSync

--- binary/190-reverse-bits/reverse-bits.py
+++ binary/190-reverse-bits/reverse-bits.py
@@ -1,5 +1,5 @@
 class Solution:
-    def reverseBits(self, n: int) -> int:
+    def reverseBits2(self, n: int) -> int:
         for i in range(16):
             left = (n >> (31 - i )) & 1
             right = (n >> i) & 1
@@ -8,3 +8,12 @@ class Solution:
                 n = n ^ (1 << (31 - i ))
                 n = n ^ (1 << i)
         return n
+        
+    def reverseBits(self, n:int)->int:
+        ans=0
+        for i in range(32):
+            ans=ans<<1
+            ans=ans|(n & 1)
+            n=n>>1
+        return ans
+
