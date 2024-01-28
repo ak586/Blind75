eb19f55a2920aeea4b341caf772a6aa148cd3147
Ajit Kumar <78835344+ak586@users.noreply.github.com>
2024-01-28 12:08:16 +0530

    Time: 27 ms (97.62%) | Memory: 16.6 MB (62.61%) - LeetSync

--- binary/190-reverse-bits/reverse-bits.py
+++ binary/190-reverse-bits/reverse-bits.py
@@ -1,10 +1,10 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         for i in range(16):
-            left = (n >> (32 - i - 1)) & 1
+            left = (n >> (31 - i )) & 1
             right = (n >> i) & 1
             if left != right:
                 # Flip the bits
-                n = n ^ (1 << (32 - i - 1))
+                n = n ^ (1 << (31 - i ))
                 n = n ^ (1 << i)
         return n
