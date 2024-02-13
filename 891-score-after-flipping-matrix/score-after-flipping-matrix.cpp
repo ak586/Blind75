583fb4f6e81565b697724e3a2fb5313bd1b8b8d1
Ajit Kumar <78835344+ak586@users.noreply.github.com>
2024-02-13 11:03:07 +0530

    Time: 5 ms (33.11%) | Memory: 10.2 MB (31.91%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
@@ -1,6 +1,43 @@
 class Solution {
 public:
-    void handleRows(vector<vector<int>>&grid){
+   
+   int matrixScore(vector<vector<int>> &grid){
+       int row=grid.size();
+       int col=grid[0].size();
+       int ans=row*(1<<(col-1));
+
+       for (int j=1; j<col; j++){
+           int count1=0;
+           for(int i=0; i<row; i++){
+               if(grid[i][0]==grid[i][j])
+               count1++;
+           }
+           int count0=row-count1;
+           count1=max(count1, count0);
+           ans+=count1*(1<<(col-j-1));
+       }
+       return ans;
+   }
+
+    // int matrixScore(vector<vector<int>>& grid) {
+    // //     handleRows(grid);
+    // //     handleCols(grid);
+
+    // // int ans=0;
+    // //     for(auto row: grid){
+    // //         // cout<<convertRowToNumber(row);
+    // //         ans+=convertRowToNumber(row);  
+    // //     }
+    // //     return ans;
+    // // make all the first column bits as one;
+    
+    
+
+    // }
+
+// intution
+
+     void handleRows(vector<vector<int>>&grid){
         int m=grid.size();
         for(int i=0; i<m; i++){
             if(grid[i][0]==0){
@@ -44,16 +81,4 @@ public:
         return ans;
     }
 
-
-    int matrixScore(vector<vector<int>>& grid) {
-        handleRows(grid);
-        handleCols(grid);
-
-    int ans=0;
-        for(auto row: grid){
-            // cout<<convertRowToNumber(row);
-            ans+=convertRowToNumber(row);  
-        }
-        return ans;
-    }
 };
