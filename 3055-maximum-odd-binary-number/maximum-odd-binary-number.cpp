2d9650543082c1b9b66d3d801fc3a7f121e51329
Ajit Kumar <78835344+ak586@users.noreply.github.com>
2024-03-01 11:23:29 +0530

    Time: 7 ms (31.42%) | Memory: 8.3 MB (63.14%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -1,34 +1,13 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
-        // int left=0;
-        // int right=s.size()-1;
-        // int firstOne=right;
-        // while(firstOne>=0){
-        //     if(s[firstOne]=='1') break;
-        //     firstOne--;
-        // }
-
-        // swap(s[firstOne], s[right]);
-        // right--;
-        // while(left<right){
-        //     if(s[right]=='1'){
-        //         while(left<right && s[left]=='1') left++;
-        //         swap(s[right], s[left]);
-        //         left++;
-        //     }
-        //     right--;
-        // }
-        // return s;
         int one=0;
         
         for(auto &ch: s){
             if(ch=='1') one++;
             ch='0';
         }
-        // cout<<one<<endl;
         s[s.size()-1]='1';
-        // cout<<s<<endl;
         --one;
         for(int i=0; i<one; i++){
             s[i]='1';
