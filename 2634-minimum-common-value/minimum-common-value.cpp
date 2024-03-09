b4863b52600158037862b56540da17e02d931c48
Ajit Kumar <78835344+ak586@users.noreply.github.com>
2024-03-09 12:36:13 +0530

    Time: 144 ms (10.91%) | Memory: 69.2 MB (14.66%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -3,8 +3,6 @@ public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         set<int> st;
         for(auto x: nums1) st.insert(x);
-        int ans=INT_MAX;
-        bool commonExists=false;
         for(auto y: nums2){
             if(st.find(y)!=st.end()){
                 return y;
