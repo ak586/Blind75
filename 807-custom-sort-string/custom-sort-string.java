78ee204c9dfc2339ea675a89161f7687d4fa42a4
Ajit Kumar <78835344+ak586@users.noreply.github.com>
2024-03-11 12:13:28 +0530

    Time: 4 ms (24.18%) | Memory: 41.5 MB (62.59%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.java
+++ 807-custom-sort-string/custom-sort-string.java
@@ -2,24 +2,26 @@ class Solution {
     public String customSortString(String order, String s) {
         char ord[]=order.toCharArray();
         char arr2[]=s.toCharArray();
-        HashMap<Character, Integer> map=new HashMap<>();
+        // HashMap<Character, Integer> map=new HashMap<>();
          Character arr[]= new Character[arr2.length];
  
         for (int i = 0; i < arr2.length; i++) {
             arr[i] = arr2[i];
         }
 
-        for (char ch: order.toCharArray()) {
-            map.put(ch,order.indexOf(ch)+1);
-        }
+        // for (char ch: order.toCharArray()) {
+        //     map.put(ch,order.indexOf(ch)+1);
+        // }
 
-        Arrays.sort(arr, new Comparator<Character>() {
+
+    Arrays.sort(arr, new Comparator<Character>() {
     public int compare(Character c1, Character c2) {
-        int x = map.getOrDefault(c1, 28);
-        int y = map.getOrDefault(c2, 28);
+        int x = order.indexOf(c1)+1;
+        int y = order.indexOf(c2)+1;
         return Integer.compare(x, y);
     }
     });
+
     StringBuilder sb
             = new StringBuilder(arr.length);
         for (Character c : arr)
