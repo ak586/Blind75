f8daf4d4f8982fba59fa6321329713da44a18c22
Ajit Kumar <78835344+ak586@users.noreply.github.com>
2024-03-24 16:44:44 +0530

    Time: 162 ms (8.35%) | Memory: 57.8 MB (63.52%) - LeetSync

---
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int longestPalindrome(String[] words) {
+        StringBuilder sb=new StringBuilder("hii");
+        System.out.println(sb.reverse());
+        Map<String, Integer> map=new HashMap<>();
+        for(String str: words){
+            map.put(str, map.getOrDefault(str,0)+1);
+        }
+        int ans=0;
+        int duplicate_count=0;
+        for(int i=0; i<words.length; i++){
+            StringBuilder temp=new StringBuilder(words[i]);
+            String reversed=new String(temp.reverse());
+            if(map.get(words[i])==0) continue;
+            if(reversed.equals(words[i])){
+            if(map.get(words[i])>=2){
+                     map.put(words[i], map.get(words[i])-2);
+                     ans+=4;
+                }
+                 else if(duplicate_count==0){
+                    map.put(words[i], map.get(words[i])-1);
+                    ans+=2;
+                    duplicate_count++;
+                }
+                continue;
+            }
+            if(map.getOrDefault(reversed,0)<=0 || map.get(words[i])<=0) continue;
+            map.put(words[i], map.get(words[i])-1);
+            map.put(reversed, map.get(reversed)-1);
+            ans+=4;
+        }
+        return ans;
+}
+}
