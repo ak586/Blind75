2b575883474f6166e475bfebdf31e87d4b348d6a
Ajit Kumar <78835344+ak586@users.noreply.github.com>
2024-04-03 09:02:22 +0530

    Time: 1059 ms (5.06%) | Memory: 45.4 MB (5.55%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                boolean visited[][]=new boolean[board.length][board[0].length];
+                if(board[i][j]==word.charAt(0) &&helper(i,j,board,"",word, visited)) return true;
+            }
+        }
+        return false;
+    }
+    private boolean helper(int i, int j, char [][]board, String curr , String word, boolean visited[][]){
+        if(word.equals(curr)) return true;
+        if(i<0 || j<0 || i>=board.length || j>=board[0].length || visited[i][j]) return false;
+        if(board[i][j]!=word.charAt(curr.length())) return false;
+        visited[i][j]=true;
+            boolean ans1=helper(i+1, j, board, curr+board[i][j], word, visited);
+            boolean ans2=helper(i, j+1, board, curr+board[i][j], word, visited);
+            boolean ans3=helper(i, j-1, board, curr+board[i][j], word, visited);
+            boolean ans4=helper(i-1, j, board, curr+board[i][j], word, visited);
+            visited[i][j]=false; 
+            return ans1||ans2||ans3||ans4; 
+        
+    }
+}
