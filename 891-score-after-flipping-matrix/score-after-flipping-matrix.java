1b4e4c30e323573b32bfe669490273e66430d7c2
Ajit Kumar <78835344+ak586@users.noreply.github.com>
2024-05-13 11:01:10 +0530

    Time: 9 ms (5.45%) | Memory: 42.2 MB (7.88%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.java
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.java
@@ -2,10 +2,12 @@ class Solution {
     public int matrixScore(int[][] grid) {
         int row=grid.length;
        int col=grid[0].length;
+    //   first col every value should be made one
        int ans=row*(1<<(col-1));
-
+        
        for (int j=1; j<col; j++){
            int count1=0;
+           // maximize number of ones in every other column
            for(int i=0; i<row; i++){
                if(grid[i][0]==grid[i][j])
                count1++;
@@ -13,6 +15,7 @@ class Solution {
            int count0=row-count1;
            count1=Math.max(count1, count0);
            ans+=count1*(1<<(col-j-1));
+           System.out.println(count1+" "+j);
        }
        return ans;
     }
